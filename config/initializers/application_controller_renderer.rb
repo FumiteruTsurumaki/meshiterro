ee5749b8e94af5a0e383395b8b7a31d25efa18d7
FumiteruTsurumaki <runprotsuru@gmail.com>
2020-11-28 05:19:31 +0000

    [Add]画像投稿機能作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '8d2e482d2c636f6ec712298f2f2414f9f3fb1ae6026179722401849f6b4b0323df5e16ffcefaf73b79f9c2ec6121148842d8bd96d0928af9effce1f304028c70'
